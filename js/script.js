97bbf2fbf7ee7f47e18992556b36d1753d5e33ad
LarryTheFatCat@users.noreply.github.com <83801755+LarryTheFatCat@users.noreply.github.com>
2022-11-02 23:57:56 -0400

    add error message html code, will continue tmrw.

--- js/script.js
+++ js/script.js
@@ -1,17 +1,1 @@
-// Grab variables to modify.
-const togglePassword = document.querySelector("#eye-one");
-const password = document.querySelector("#pwd");
-
-
-/**
- * On the event that a user tries to click the eye icon, it triggers the function.
- */
-togglePassword.addEventListener("click", function() {
-    // If the type is password, then set it to text, otherwise, set it to password.
-    const type = password.getAttribute("type") === "password" ? "text" : "password"; 
-    password.setAttribute("type", type);
-
-    // toggle icon
-    this.classList.toggle("bi-eye-slash-fill");
-});
-
+// function to check for authentication across all places, includes 
